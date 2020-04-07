073a9651b580fb334adac72c8b98c9009f52eab4
Edson De Araujo <39536164+edsondearaujo@users.noreply.github.com>
2020-04-07 00:23:40 -0300

    text box replacement

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 
 class Home extends StatefulWidget {
@@ -8,72 +9,101 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF363435),
-      appBar: AppBar(
-        title: Text("Mobile GMS",
-          style: TextStyle(
-              fontSize: 20,
-              color: Color(0xFF00b8ea)
+        backgroundColor: Color(0xFF3f3f95),
+        appBar: AppBar(
+          title: Text("Mobile GMS",
+            style: TextStyle(
+                fontSize: 20,
+                color: Color(0xFF008d35)
+            ),
           ),
-        ),
-        backgroundColor: Color(0xFF363435),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            color: Color(0xFF00b8ea),
-            onPressed: (){},
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Color(0xFF00b8ea),
-            border: Border.all(width: 3, color: Color(0xFF363435)),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "nome da imagem"
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-
-              children: <Widget>[
-                Expanded(
-                  child: Image.asset("image/logo.png",
-                  //width: 200,
-                  //height: 150,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                BottomAppBar(
-                  color: Color(0xFF363435),
-
-                )
-              ],
+          backgroundColor: Color(0xFF3f3f95),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              color: Color(0xFF008d35),
+              onPressed: (){},
             )
           ],
         ),
-      )
+        body: Container(
+          padding: EdgeInsets.all(16),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Color(0xFF3f3f95),
+            border: Border.all(width: 3, color: Color(0xFF008d35)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: TextField(
+                            decoration: InputDecoration(
+                            hintText: 'image name'
+                            )
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset("images/logo.jpg",
+                    width: 320,
+                    height: 320,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const MaterialButton(onPressed: null,
+                    elevation: 20,
+                    color: Colors.red,
+                    child: Text('Back',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xFF008d35),
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const MaterialButton(onPressed: null,
+                        elevation: 20,
+                        color: Colors.red,
+                        child: Text('Next',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color(0xFF008d35),
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      )
+
+                    ],
+                  )
+                ],
+              ),
+          ]
+          ),
+        )
     );
   }
 }
